480505ff4a58e23c517322fc3fdfd17475c9e4a1
Giannis <gtsol@tsolakis.dev>
2026-06-28 20:44:36 +0300

    Fix schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS readings (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    timestamp   INTEGER    NOT NULL,
+
     temperature REAL    NOT NULL,
     humidity    REAL    NOT NULL,
     pressure    REAL,
-    bat_voltage REAL    NOT NULL,
+    bat_voltage REAL    NOT NULL
 
-    timestamp   TEXT    NOT NULL
 );
